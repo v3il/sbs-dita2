0ad853c4344eecb2aaeffecafc735944e349f3a0
K1Koff <111696140+K1Koff@users.noreply.github.com>
2023-02-19 23:33:53 +0200

    hero mana & hp bar view v1

---
+++ src/views/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,49 @@
+import progressBarTempalate from './progressBarTempalate.html?raw';
+import { ComponentView } from '../ComponentView';
+
+export class ProgressBar extends ComponentView {
+    constructor({
+        player, playerBoardSide, parentView, el
+    }) {
+        super({ parentView, el });
+
+        this.playerBoardSide = playerBoardSide;
+        this.player = player;
+
+        this.manaBar = null;
+        this.hpBar = null;
+        this.manaValue = null;
+        this.hpValue = null;
+    }
+
+    updateProgressBars() {
+        const maxHP = this.player.hero.maxHitPoints;
+        const currHP = this.player.hero.hitPoints;
+        const maxMP = this.player.hero.maxManaPoints;
+        const currMP = this.player.hero.manaPoints;
+        const relativeMP = Math.round((currMP / maxMP) * 100);
+        const relativeHP = Math.round((currHP / maxHP) * 100);
+
+        this.manaBar.style.setProperty('--width', `${relativeMP}%`);
+        this.hpBar.style.setProperty('--width', `${relativeHP}%`);
+
+        this.manaValue.textContent = `${currMP}/${maxMP}`;
+        this.hpValue.textContent = `${currHP}/${maxHP}`;
+    }
+
+    init() {
+        this.manaBar = this.playerBoardSide.querySelector('[data-mana-bar]');
+        this.manaValue = this.manaBar.firstChild;
+        this.manaBar.className = `${this.player.team}ManaPointsContainer progressBar`;
+
+        this.hpBar = this.playerBoardSide.querySelector('[data-hp-bar]');
+        this.hpValue = this.hpBar.firstChild;
+        this.hpBar.className = `${this.player.team}HitPointsContainer progressBar`;
+    }
+
+    render() {
+        super.render(progressBarTempalate);
+        this.init();
+        this.updateProgressBars();
+    }
+}
--- src/views/pages/GameBoardPage/GameBoardPage.js
+++ src/views/pages/GameBoardPage/GameBoardPage.js
@@ -1,15 +1,87 @@
 import { PageView } from '../PageView';
+import { ProgressBar } from '../../components/ProgressBar/ProgressBar';
 import template from './template.html?raw';
+import { game } from '../../../models';
+import { DotaAssetUrlManager } from '../../../services/DotaAssetUrlManager';
 import './style.css';
 
 export class GameBoardPage extends PageView {
     constructor() {
         super(template);
+
+        this.radiantTeam = {};
+        this.direTeam = {};
+        this.radiantHeroProgressBar = null;
+        this.direHeroProgressBar = null;
+    }
+
+    teamsInit() {
+        this.radiantTeam = {
+            player: game.radiantPlayer,
+            view: document.querySelector('[data-radiant-player-view]')
+        };
+
+        this.direTeam = {
+            player: game.direPlayer,
+            view: document.querySelector('[data-dire-player-view]')
+        };
+
+        this.radiantHeroProgressBar = new ProgressBar({
+            player: game.radiantPlayer,
+            playerBoardSide: this.radiantTeam.view,
+            parentView: this,
+            el: this.radiantTeam.view.querySelector('[data-progress-bar]')
+        });
+
+        this.direHeroProgressBar = new ProgressBar({
+            player: game.direPlayer,
+            playerBoardSide: this.direTeam.view,
+            parentView: this,
+            el: this.direTeam.view.querySelector('[data-progress-bar]')
+        });
+    }
+
+    mountPlayerStatusBar(teams) {
+        teams.forEach(({ player, view }) => {
+            const statusBarContainer = view.querySelector(`[data-${player.team}-status-bar]`);
+            const nameElement = document.createElement('div');
+            nameElement.classList = `data-name-${player.team}`;
+            nameElement.innerHTML = `${player.name} - team ${player.team}`.toUpperCase();
+            view.replaceChild(nameElement, statusBarContainer);
+        });
+    }
+
+    mountPlayerHeroAvatar(teams) {
+        teams.forEach(({ player, view }) => {
+            const avatarContainer = view.querySelector(`[data-${player.team}-hero-avatar]`);
+            const avatarURL = DotaAssetUrlManager.getHeroAvatarUrl(player.hero.id);
+            const heroAvatarElement = document.createElement('div');
+            const avatarPicture = document.createElement('img');
+
+            avatarPicture.src = avatarURL;
+            avatarPicture.classList = `${player.team}-avatar `;
+            heroAvatarElement.classList = `data-hero-avatar-${player.team}`;
+
+            if (player.hero.isLeftAvatarDirection && player.isRadiant) {
+                avatarPicture.style.transform = 'scale(-1, 1)';
+            }
+
+            if (!player.hero.isLeftAvatarDirection && !player.isRadiant) {
+                avatarPicture.style.transform = 'scale(-1, 1)';
+            }
+
+            heroAvatarElement.append(avatarPicture);
+            view.replaceChild(heroAvatarElement, avatarContainer);
+        });
     }
 
     render(mountingEl) {
         super.render(mountingEl);
+        this.teamsInit();
 
-        // Render other components, listen DOM events, etc.
+        this.mountPlayerStatusBar([this.direTeam, this.radiantTeam]);
+        this.mountPlayerHeroAvatar([this.direTeam, this.radiantTeam]);
+        this.radiantHeroProgressBar.render();
+        this.direHeroProgressBar.render();
     }
 }
