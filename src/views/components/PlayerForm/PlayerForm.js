164dc6f27928c0a6849c36121edf4d1c5008d3bb
K1Koff <111696140+K1Koff@users.noreply.github.com>
2023-03-20 23:51:33 +0200

    PlayerForm v2.0

--- src/views/components/PlayerForm/PlayerForm.js
+++ src/views/components/PlayerForm/PlayerForm.js
@@ -1,16 +1,13 @@
-/* eslint-disable max-len */
-import heroAvatarTemplate from './heroAvatarTemplate.html?raw';
-import template from './template.html?raw';
-import { HeroStats } from '../HeroStat/HeroStat';
-import { ComponentView } from '../ComponentView';
 import './styles.scss';
+import template from './template.html?raw';
+import { ComponentView } from '../ComponentView';
 import { heroFactory } from '../../../services';
-import { DotaAssetUrlManager } from '../../../services/DotaAssetUrlManager';
+import { HeroStats } from '../HeroStat/HeroStat';
+import { HeroSelector } from '../PlayerFormHeroAvatar/HeroSelector';
+import { Decorations } from '../../../consts/Derorations';
 
 export class PlayerForm extends ComponentView {
     #player;
-    #idOfHeroSelected;
-    #elementSelectedHero;
     heroStatsContainer;
     heroSelectorContainer;
     inputField;
@@ -20,121 +17,119 @@ export class PlayerForm extends ComponentView {
     constructor({ player, parentView, el }) {
         super({ parentView, el });
 
-        this.#idOfHeroSelected = null;
         this.#player = player;
-        this.#elementSelectedHero = null;
         this.el = el;
+
+        this.heroAvatar = null;
+        this.heroStats = null;
+        this.selectedHero = null;
+        this.isHeroChosen = false;
+        this.isPlayerNameInputed = true;
+
         this.render();
-    }
-
-    initPageElements() {
-        this.heroStatsContainer = this.el.querySelector('[data-hero-stat]');
-        this.heroSelectorContainer = this.el.querySelector('[data-hero-selector]');
-        this.inputField = this.el.querySelector('[data-player-name]');
-        this.validationButton = this.el.querySelector('[data-confirm]');
-        this.heroStats = new HeroStats({
-            parentView: this,
-            el: this.heroStatsContainer
-        });
-        if (this.#player.team === 'radiant') {
-            this.inputField.value = 'player 1';
-        } else {
-            this.inputField.value = 'player 2';
-        }
-    }
-
-    getNameValidation() {
-        this.validationButton.disabled = true;
-
-        this.inputField.addEventListener('input', () => {
-            if (this.inputField.value !== '') {
-                this.inputField.style.boxShadow = '0 0 5px #00ffeab7, 0 0 25px #00ffeab7, 0 0 50px #00ffeab7, 0 0 200px #00ffeab7';
-            } else { this.inputField.style.boxShadow = ''; }
-
-            if ((this.inputField.value !== '') && this.#idOfHeroSelected !== null) {
-                this.validationButton.disabled = false;
-                this.validationButton.style.boxShadow = '0 0 5px #00ffeab7, 0 0 25px #00ffeab7, 0 0 50px #00ffeab7, 0 0 200px #00ffeab7';
-            } else {
-                this.validationButton.disabled = true;
-                this.validationButton.style.boxShadow = '';
-            }
-        });
-    }
-
-    renderHeroIcons() {
-        const heroes = heroFactory.availableHeroes;
-
-        heroes.forEach((hero) => {
-            const currentHeroAvatarURL = DotaAssetUrlManager.getHeroImageUrl(hero.id);
-            const heroAvatarButton = document.createElement('button');
-
-            heroAvatarButton.innerHTML = heroAvatarTemplate;
-
-            const avatarImage = heroAvatarButton.querySelector('[data-hero-avatar]');
-
-            heroAvatarButton.setAttribute('class', 'hero-avatar-button');
-            avatarImage.src = currentHeroAvatarURL;
-
-            heroAvatarButton.addEventListener('click', () => {
-                this.heroStats.showStats(hero);
-                this.#idOfHeroSelected = hero.id;
-
-                if (this.#elementSelectedHero) {
-                    this.#elementSelectedHero.setAttribute('class', 'hero-avatar-button');
-                }
-                this.#elementSelectedHero = heroAvatarButton;
-                this.#elementSelectedHero.setAttribute('class', 'hero-avatar-button-active');
-
-                if (this.inputField.value !== '') {
-                    this.validationButton.disabled = false;
-                    this.validationButton.style.boxShadow = '0 0 5px #00ffeab7, 0 0 25px #00ffeab7, 0 0 50px #00ffeab7, 0 0 200px #00ffeab7';
-                }
-            });
-
-            this.heroSelectorContainer.appendChild(heroAvatarButton);
-        });
-    }
-
-    getHeroConfirmation() {
-        this.validationButton.addEventListener('click', () => {
-            if (this.#idOfHeroSelected) {
-                const selectedHero = heroFactory.createHero(this.#idOfHeroSelected, this.#player.events);
-
-                this.validationButton.disabled = true;
-                this.inputField.disabled = true;
-                this.validationButton.style.boxShadow = '';
-                this.disableHeroAvatars();
-                this.#player.setName(this.inputField.value);
-                this.#player.setHero(selectedHero);
-                this.emit('selected');
-            }
-        });
-    }
-
-    disableHeroAvatars() {
-        Array.from(this.heroSelectorContainer.children).forEach((node) => {
-            const currentElement = node;
-
-            currentElement.disabled = true;
-        });
     }
 
     render() {
         this.mountElement(template);
         this.initPageElements();
+        this.setTitle();
+        this.setPlayerFormEvents();
+        this.handleInputField();
+        this.handleValidationButton();
+    }
 
-        const title = this.el.querySelector('[data-title]');
+    initPageElements() {
+        this.title = this.el.querySelector('[data-title]');
+        this.inputField = this.el.querySelector('[data-player-name]');
+        this.heroSelectorContainer = this.el.querySelector('[data-hero-selector]');
+        this.heroStatsContainer = this.el.querySelector('[data-hero-stat]');
+        this.validationButton = this.el.querySelector('[data-confirm]');
 
-        title.innerHTML = this.#player.team.toUpperCase();
-        switch (this.#player.team) {
-        case 'dire': title.setAttribute('class', 'player-form__title red');
-            break;
-        default: title.setAttribute('class', 'player-form__title blue');
-            break;
+        this.heroSelector = new HeroSelector({
+            playerFormContainer: this.el,
+            parentView: this,
+            el: this.heroSelectorContainer
+        });
+
+        this.heroStats = new HeroStats({
+            parentView: this,
+            el: this.heroStatsContainer
+        });
+    }
+
+    setTitle() {
+        this.title.innerHTML = this.#player.team.toUpperCase();
+        if (this.#player.team === 'radiant') {
+            this.title.className = 'player-form__title red';
+        } else {
+            this.title.className = 'player-form__title blue';
         }
+    }
 
-        this.renderHeroIcons();
-        this.getHeroConfirmation();
-        this.getNameValidation();
+    setPlayerFormEvents() {
+        this.on('chooseHero', ({ hero }) => {
+            this.selectedHero = heroFactory.createHero(hero.id, this.#player.events);
+
+            this.isHeroChosen = true;
+            this.heroStats.showStats(hero);
+
+            this.emit('setAvatarButtonState-Passive');
+            this.emit(`setAvatarButtonState-Active-${hero.id}`);
+            this.emit('checkValidationButtonState');
+        });
+    }
+
+    handleInputField() {
+        if (this.#player.team === 'radiant') {
+            this.inputField.value = 'player 1';
+        } else {
+            this.inputField.value = 'player 2';
+        }
+        this.inputField.style.boxShadow = Decorations.boxShadowActive;
+
+        this.inputField.addEventListener('input', () => {
+            if (this.inputField.value !== '') {
+                this.isPlayerNameInputed = true;
+                this.inputField.style.boxShadow = Decorations.boxShadowActive;
+            } else {
+                this.isPlayerNameInputed = false;
+                this.inputField.style.boxShadow = '';
+            }
+            this.emit('checkValidationButtonState');
+        });
+    }
+
+    handleValidationButton() {
+        this.disableElement(this.validationButton);
+
+        this.on('checkValidationButtonState', () => {
+            if (this.isHeroChosen && this.isPlayerNameInputed) {
+                this.enableElement(this.validationButton);
+            } else {
+                this.disableElement(this.validationButton);
+            }
+        });
+
+        this.validationButton.addEventListener('click', () => {
+            this.#player.setHero(this.selectedHero);
+            this.#player.setName(this.inputField.value);
+
+            this.disableElement(this.validationButton);
+            this.disableElement(this.inputField);
+            this.emit('disableAllAvatarButtons');
+            this.emit('selected');
+        });
+    }
+
+    enableElement(elem) {
+        const element = elem;
+
+        element.disabled = false;
+    }
+
+    disableElement(elem) {
+        const element = elem;
+
+        element.disabled = true;
     }
 }
