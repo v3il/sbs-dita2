1a42c73f76661862a8213826be39512a5de777a6
K1Koff <111696140+K1Koff@users.noreply.github.com>
2023-03-31 21:44:08 +0300

    made player events independent

--- src/models/index.js
+++ src/models/index.js
@@ -3,9 +3,7 @@ import { Player } from './Player';
 import { TeamNames } from '../consts/TeamNames';
 import { Game } from './Game';
 
-const events = new EventEmitter();
+const radiantPlayer = new Player({ events: new EventEmitter(), team: TeamNames.RADIANT });
+const direPlayer = new Player({ events: new EventEmitter(), team: TeamNames.DIRE });
 
-const radiantPlayer = new Player({ events, team: TeamNames.RADIANT });
-const direPlayer = new Player({ events, team: TeamNames.DIRE });
-
-export const game = new Game({ radiantPlayer, direPlayer, events });
+export const game = new Game({ radiantPlayer, direPlayer, events: new EventEmitter() });
