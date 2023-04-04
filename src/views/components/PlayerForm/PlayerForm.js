8ce28da6a4957dc491747a70a2ad12b0f1abc168
K1Koff <111696140+K1Koff@users.noreply.github.com>
2023-04-05 00:03:56 +0300

    added hero preselect feature

--- src/views/components/PlayerForm/PlayerForm.js
+++ src/views/components/PlayerForm/PlayerForm.js
@@ -3,16 +3,10 @@ import template from './template.html?raw';
 import { ComponentView } from '../ComponentView';
 import { heroFactory } from '../../../services';
 import { HeroStats } from '../HeroStat/HeroStat';
-import { HeroSelector } from '../PlayerFormHeroAvatar/HeroSelector';
-import { Decorations } from '../../../consts/Derorations';
+import { HeroSelector } from '../HeroSelector/HeroSelector';
 
 export class PlayerForm extends ComponentView {
     #player;
-    heroStatsContainer;
-    heroSelectorContainer;
-    inputField;
-    validationButton;
-    heroStats;
 
     constructor({ player, parentView, el }) {
         super({ parentView, el });
@@ -36,24 +30,23 @@ export class PlayerForm extends ComponentView {
         this.setPlayerFormEvents();
         this.handleInputField();
         this.handleValidationButton();
+        this.preselectHero();
     }
 
     initPageElements() {
         this.title = this.el.querySelector('[data-title]');
         this.inputField = this.el.querySelector('[data-player-name]');
-        this.heroSelectorContainer = this.el.querySelector('[data-hero-selector]');
-        this.heroStatsContainer = this.el.querySelector('[data-hero-stat]');
         this.validationButton = this.el.querySelector('[data-confirm]');
 
         this.heroSelector = new HeroSelector({
             playerFormContainer: this.el,
             parentView: this,
-            el: this.heroSelectorContainer
+            el: this.el.querySelector('[data-hero-selector]')
         });
 
         this.heroStats = new HeroStats({
             parentView: this,
-            el: this.heroStatsContainer
+            el: this.el.querySelector('[data-hero-stat]')
         });
     }
 
@@ -68,15 +61,19 @@ export class PlayerForm extends ComponentView {
 
     setPlayerFormEvents() {
         this.on('chooseHero', ({ hero }) => {
-            this.selectedHero = heroFactory.createHero(hero.id, this.#player.events);
-
-            this.isHeroChosen = true;
-            this.heroStats.showStats(hero);
-
-            this.emit('setAvatarButtonState-Passive');
-            this.emit(`setAvatarButtonState-Active-${hero.id}`);
-            this.checkValidationButtonState();
+            this.selectHero(hero);
         });
+    }
+
+    selectHero(hero) {
+        this.selectedHero = heroFactory.createHero(hero.id, this.#player.events);
+
+        this.isHeroChosen = true;
+        this.heroStats.showStats(hero);
+
+        this.emit('setAvatarButtonState-Passive');
+        this.emit(`setAvatarButtonState-Active-${hero.id}`);
+        this.checkValidationButtonState();
     }
 
     handleInputField() {
@@ -85,15 +82,15 @@ export class PlayerForm extends ComponentView {
         } else {
             this.inputField.value = 'player 2';
         }
-        this.inputField.style.boxShadow = Decorations.boxShadowActive;
+        this.inputField.classList = 'input input-field-active';
 
         this.inputField.addEventListener('input', () => {
             if (this.inputField.value !== '') {
                 this.isPlayerNameInputed = true;
-                this.inputField.style.boxShadow = Decorations.boxShadowActive;
+                this.inputField.classList = 'input input-field-active';
             } else {
                 this.isPlayerNameInputed = false;
-                this.inputField.style.boxShadow = '';
+                this.inputField.classList = 'input';
             }
             this.checkValidationButtonState();
         });
@@ -111,6 +108,14 @@ export class PlayerForm extends ComponentView {
             this.emit('disableAllAvatarButtons');
             this.emit('selected');
         });
+    }
+
+    preselectHero() {
+        const heroes = heroFactory.availableHeroes;
+        const preselectedHero = heroes[0];
+
+        this.selectHero(preselectedHero);
+        this.emit(`setAvatarButtonState-Active-${preselectedHero.id}`);
     }
 
     checkValidationButtonState() {
